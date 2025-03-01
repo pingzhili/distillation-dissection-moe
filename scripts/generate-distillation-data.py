95da498f5506f13377e7a7a90abf447ed4df60ea
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-01 00:48:29 -0500

    sync

--- scripts/generate-distillation-data.py
+++ scripts/generate-distillation-data.py
@@ -1,11 +1,11 @@
+import torch
 from fire import Fire
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-from transformers import (
-    set_seed, AutoTokenizer,
-)
+from transformers import set_seed, AutoTokenizer
 from ddmoe.data import batch_preprocess_fn, CustomDataCollatorWithPadding
 from ddmoe.models import DeepseekV3ForCausalLM
+from accelerate import Accelerator
 from functools import partial
 from tqdm import tqdm
 import os
@@ -30,41 +30,41 @@ def generate_distillation_data(
         save_dir: str = "data/",
         num_workers: int = 4,
 ):
-    model = DeepseekV3ForCausalLM.from_pretrained(
-        model_name,
-        torch_dtype="auto",
-        device_map="auto",
-        trust_remote_code=True,
-    )
-    model.eval()
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer.pad_token_id = tokenizer.eos_token_id
-    model.config.pad_token_id = tokenizer.pad_token_id
-    model.generation_config.pad_token_id = model.generation_config.eos_token_id[0]
-    if dataset_name == "ServiceNow-AI/R1-Distill-SFT":
-        dataset = load_dataset(
-            "ServiceNow-AI/R1-Distill-SFT", "v1", trust_remote_code=True
+    accelerator = Accelerator()
+    rank = accelerator.local_process_index
+
+    with accelerator.main_process_first(desc="Loading model"):
+        model = DeepseekV3ForCausalLM.from_pretrained(model_name, torch_dtype="auto", trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+        model.eval()
+        model.config.pad_token_id = tokenizer.pad_token_id
+        model.generation_config.pad_token_id = model.generation_config.eos_token_id[0]
+        if dataset_name == "ServiceNow-AI/R1-Distill-SFT":
+            dataset = load_dataset(
+                "ServiceNow-AI/R1-Distill-SFT", "v1", trust_remote_code=True
+            )
+        else:
+            raise ValueError(f"Dataset {dataset_name} not found.")
+        dataset = dataset["train"]
+        preprocess_fn = partial(batch_preprocess_fn, task="chat-eval", tokenizer=tokenizer)
+        dataset = dataset.map(preprocess_fn, batched=True, num_proc=num_workers, remove_columns=dataset.column_names)
+        data_collator = CustomDataCollatorWithPadding(
+            tokenizer=tokenizer, pad_to_multiple_of=8, extra_keys_to_ignore=["content"]
         )
-    else:
-        raise ValueError(f"Dataset {dataset_name} not found.")
-    dataset = dataset["train"]
-    # debugging keep only 1000
-    dataset = dataset.select(range(1000))
-    preprocess_fn = partial(batch_preprocess_fn, task="chat-eval", tokenizer=tokenizer)
-    dataset = dataset.map(preprocess_fn, batched=True, num_proc=num_workers, remove_columns=dataset.column_names)
-    data_collator = CustomDataCollatorWithPadding(
-        tokenizer=tokenizer, pad_to_multiple_of=8, extra_keys_to_ignore=["content"]
-    )
-    dataloader = DataLoader(dataset=dataset, collate_fn=data_collator, batch_size=1, num_workers=num_workers)
+        dataloader = DataLoader(dataset=dataset, collate_fn=data_collator, batch_size=1, num_workers=num_workers)
+
+    model, dataloader = accelerator.prepare(model, dataloader)
 
     # write the response into a file on-the-fly
     for batch in tqdm(dataloader, desc="Generating distillation data"):
         input_ids = batch["input_ids"].cuda()
         content = batch["content"]
-        generated_ids = model.generate(inputs=input_ids, max_new_tokens=max_length - len(input_ids[0]))
+        with torch.inference_mode():
+            generated_ids = model.generate(inputs=input_ids, max_new_tokens=max_length - len(input_ids[0]))
         generated_ids = generated_ids[:, len(input_ids[0]):]
         response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-        append_generation(response, content[0], os.path.join(save_dir, "distillation_data.jsonl"))
+        append_generation(response, content[0], os.path.join(save_dir, f"distillation_data_rank_{rank}.jsonl"))
 
 
 if __name__ == "__main__":
