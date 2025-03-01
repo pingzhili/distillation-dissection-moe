85d6e8d01208d0be8b90fba412f55c4dcfde13c7
Pingzhi Li <pingzhi@cs.unc.edu>
2025-02-28 19:03:15 -0500

    sync

--- src/ddmoe/models/deepseek/modeling_deepseek.py
+++ src/ddmoe/models/deepseek/modeling_deepseek.py
@@ -1450,14 +1450,14 @@ class DeepseekV3Model(DeepseekV3PreTrainedModel):
             )
         else:
             # 4d mask is passed through the layers
-            # print("before", attention_mask.shape, batch_size, seq_length, inputs_embeds.shape, past_key_values_length)
+            print("before", attention_mask.shape, batch_size, seq_length, inputs_embeds.shape, past_key_values_length)
             attention_mask = _prepare_4d_causal_attention_mask(
                 attention_mask,
                 (batch_size, seq_length),
                 inputs_embeds,
                 past_key_values_length,
             )
-            # print("after", attention_mask.shape)
+            print("after", attention_mask.shape)
 
         # embed positions
         hidden_states = inputs_embeds
