f334d4a3d83e3b522bf4340739a6ea3932c9ce63
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-04 04:00:19 -0500

    sync

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 export PYTHONPATH=$PYTHONPATH:src
-#export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 # python debug.py
-python scripts/generate-distillation-data.py
+#python scripts/generate-distillation-data.py
 #accelerate launch --num_processes=8 scripts/generate-distillation-data.py
-
-
 #sudo docker run -e NCCL_P2P_DISABLE=1 --gpus all --shm-size 32g -p 0.0.0.0:23333:23333 -v ~/.cache/huggingface:/root/.cache/huggingface -v /home/pingzhi/model-checkpoints:/model-checkpoints --ipc=host --network=host --name moonlight --privileged lmsysorg/sglang:latest bash -c "pip install blobfile tiktoken transformers==4.48.2 && python3 -m sglang.launch_server --model-path moonshotai/Moonlight-16B-A3B-Instruct --trust-remote-code --disable-radix-cache --tp 2 --dp 4 2>&1" | sudo tee output.log
+
+python scripts/finetune.py --base_model_name="allenai/OLMoE-1B-7B-0125" \
+  --dataset_name="Phando/sft-dataset-from-moonlight" --output_dir="outputs/olmoe-1b-7b-0125-sft"
