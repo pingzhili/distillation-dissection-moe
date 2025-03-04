9294332cfe5a1667c7e06ffd9c9b07a4c210de26
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-04 05:50:28 -0500

    sync

--- scripts/finetune.py
+++ scripts/finetune.py
@@ -147,8 +147,8 @@ def train_sft(
         for step, batch in enumerate(dataloader):
             model.train()
             with accelerator.accumulate(model):
-                if accelerator.is_local_main_process:
-                    print(batch)
+                # if accelerator.is_local_main_process:
+                #     print(batch)
                 outputs = model(**batch)
                 loss = outputs.loss
                 accelerator.backward(loss)
