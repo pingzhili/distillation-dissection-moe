05fbc8e5b195f5b5c2a2edfadb14709766e0ce8b
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-07 20:38:50 -0500

    sync

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=$PYTHONPATH:src
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1
 # python debug.py
 #python scripts/generate-distillation-data.py
 #accelerate launch --num_processes=8 scripts/generate-distillation-data.py
