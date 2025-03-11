359e872c9378ead872d6b26a2ee10830b76fdaab
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-11 18:26:00 -0400

    sync

--- dump.sh
+++ dump.sh
@@ -1,6 +1,8 @@
 export PYTHONPATH=$PYTHONPATH:src
 export CUDA_VISIBLE_DEVICES=7
+#python scripts/dump-hidden-states.py \
+#    --checkpoint_path="checkpoints/olmoe-1b-7b-0125-sft-distilled-moonlight/checkpoint-4000" --content="router_tokens"
 python scripts/dump-hidden-states.py \
-    --checkpoint_path="checkpoints/olmoe-1b-7b-0125-sft-distilled-moonlight/checkpoint-4000" --content="router_tokens"
+    --checkpoint_path="allenai/OLMoE-1B-7B-0125" --content="router_tokens"
 #python scripts/profile-routing.py \
 #    --all_checkpoint_paths="moonshotai/Moonlight-16B-A3B-Instruct,allenai/OLMoE-1B-7B-0125,checkpoints/olmoe-1b-7b-0125-sft-distilled-moonlight/checkpoint-4000"
