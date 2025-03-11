e65c01e4d66d60d4883f0095ee27e25300aef759
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-11 14:56:12 -0400

    sync

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=$PYTHONPATH:src
-export CUDA_VISIBLE_DEVICES=4,5,6,7
+export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
 #export CUDA_LAUNCH_BLOCKING=1
 #export TORCH_USE_CUDA_DSA=1
 # python debug.py
