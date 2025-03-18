1807327d134c9aaa0ba7886f7cea43e851ca6226
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-18 18:57:10 -0400

    sync

--- dump.sh
+++ dump.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=$PYTHONPATH:src
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
 #python scripts/dump-hidden-states.py \
 #    --checkpoint_path="checkpoints/olmoe-1b-7b-0125-sft-distilled-moonlight/checkpoint-4000" --content="router_tokens" \
 #    --save_dir="./outputs/profiling/olmoe-distill-4000"
