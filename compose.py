e7d68930b9737e553414a40dc4d96f6cd2897cd1
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-21 11:51:54 -0400

    sync

--- compose.py
+++ compose.py
@@ -21,6 +21,7 @@ sft_full = sft_full.add_column("source", source_list)
 sft_full = sft_full.add_column("id", id_list)
 sft_full = sft_full.add_column("source_dataset", source_dataset_list)
 
+print(sft_full["source_dataset"].unique())
 print(f"Before filtering, the number of samples in sft_full is {len(sft_full)}")
 # remove the samples with "source" is "ai2-adapt-dev/tulu_hard_coded_repeated_10"
 sft_full = sft_full.filter(lambda example: example["source"] != "ai2-adapt-dev/tulu_hard_coded_repeated_10")
