2d66cb1ea3ddefad65e856bd763cda2755ee84a7
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-21 11:52:37 -0400

    sync

--- compose.py
+++ compose.py
@@ -1,31 +1,33 @@
 from datasets import load_dataset
-from tqdm import tqdm
 
-original = load_dataset("ServiceNow-AI/R1-Distill-SFT", "v1", split="train")
+#
+# original = load_dataset("ServiceNow-AI/R1-Distill-SFT", "v1", split="train")
+# sft_full = load_dataset("Phando/sft-dataset-from-moonlight", split="train")
+#
+# question_to_sample = {
+#     sample["messages"][0]["content"]: sample for sample in tqdm(original, desc="Building question_to_sample mapping")
+# }
+# source_list = []
+# source_dataset_list = []
+# id_list = []
+# for sample in tqdm(sft_full, desc="Processing the samples in sft_full"):
+#     question = sample["question"]
+#     full_sample = question_to_sample[question]
+#     source_list.append(full_sample["source"])
+#     source_dataset_list.append(full_sample["source_dataset"])
+#     id_list.append(full_sample["id"])
+#
+# sft_full = sft_full.add_column("source", source_list)
+# sft_full = sft_full.add_column("id", id_list)
+# sft_full = sft_full.add_column("source_dataset", source_dataset_list)
+#
+# print(f"Before filtering, the number of samples in sft_full is {len(sft_full)}")
+# # remove the samples with "source" is "ai2-adapt-dev/tulu_hard_coded_repeated_10"
+# sft_full = sft_full.filter(lambda example: example["source"] != "ai2-adapt-dev/tulu_hard_coded_repeated_10")
+# print(f"After filtering, the number of samples in sft_full is {len(sft_full)}")
+#
+# sft_full.push_to_hub("Phando/sft-dataset-from-moonlight-noid")
+
 sft_full = load_dataset("Phando/sft-dataset-from-moonlight", split="train")
-
-question_to_sample = {
-    sample["messages"][0]["content"]: sample for sample in tqdm(original, desc="Building question_to_sample mapping")
-}
-source_list = []
-source_dataset_list = []
-id_list = []
-for sample in tqdm(sft_full, desc="Processing the samples in sft_full"):
-    question = sample["question"]
-    full_sample = question_to_sample[question]
-    source_list.append(full_sample["source"])
-    source_dataset_list.append(full_sample["source_dataset"])
-    id_list.append(full_sample["id"])
-
-sft_full = sft_full.add_column("source", source_list)
-sft_full = sft_full.add_column("id", id_list)
-sft_full = sft_full.add_column("source_dataset", source_dataset_list)
-
 print(sft_full["source_dataset"].unique())
 print(sft_full["source"].unique())
-print(f"Before filtering, the number of samples in sft_full is {len(sft_full)}")
-# remove the samples with "source" is "ai2-adapt-dev/tulu_hard_coded_repeated_10"
-sft_full = sft_full.filter(lambda example: example["source"] != "ai2-adapt-dev/tulu_hard_coded_repeated_10")
-print(f"After filtering, the number of samples in sft_full is {len(sft_full)}")
-
-# sft_full.push_to_hub("Phando/sft-dataset-from-moonlight-noid")
