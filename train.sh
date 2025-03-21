9eaaf41bd1c8bd75beefc87771d08277f1d5e7d4
Pingzhi Li <pingzhi@cs.unc.edu>
2025-03-21 15:04:58 -0400

    sync

--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 export PYTHONPATH=$PYTHONPATH:src
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export NCCL_P2P_DISABLE=1
 #export CUDA_LAUNCH_BLOCKING=1
 #export TORCH_USE_CUDA_DSA=1
 # python debug.py
