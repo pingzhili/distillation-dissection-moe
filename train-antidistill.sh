7e362077dc4784711c4830a126a09633d4b45cd8
Pingzhi Li <pingzhi@cs.unc.edu>
2025-04-23 16:40:45 -0400

    sync

--- train-antidistill.sh
+++ train-antidistill.sh
@@ -2,7 +2,7 @@ export PYTHONPATH=$PYTHONPATH:src
 export CUDA_VISIBLE_DEVICES=0,1
 export NCCL_P2P_DISABLE=1
 
-KD_COEF=${1:-0.01}
+KD_COEF=${1:-0.001}
 KD_TEMP=1.0
 EPOCH=3
 OUTPUT_DIR="outputs/qwen7b-antidistill-coef$KD_COEF-temp$KD_TEMP-epoch$EPOCH"
@@ -19,5 +19,5 @@ accelerate launch --config_file configs/zero3-2gpu-ga32.yaml \
     --output_dir=$OUTPUT_DIR \
     --num_train_epochs=$EPOCH \
     --batch_size_per_device=2 \
-    --gradient_accumulation_steps=32 \
-    --debugging=True
+    --gradient_accumulation_steps=32
+#    --debugging=True
