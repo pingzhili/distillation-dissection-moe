50b8332c5f5a30ad9aed12edb0e0b5e355a018d0
Pingzhi Li <pingzhi@cs.unc.edu>
2025-05-01 05:34:21 -0400

    update

--- run-gen-gsm8k.sh
+++ run-gen-gsm8k.sh
@@ -1,5 +1,12 @@
 export PYTHONPATH=$PYTHONPATH:src
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=0
+
+# MODEL_PATH="outputs/qwen7b-antidistill-coef0.00003-temp2-epoch2-lr5e-5/checkpoint-60"
+# python scripts/generate-distillation-data-offline.py \
+#     --model_name="$MODEL_PATH" \
+#     --dataset_name="openai/gsm8k" \
+#     --save_dir="$MODEL_PATH/gen-gsm8k"
+
 
 MODEL_PATH="outputs/qwen7b-antidistill-coef0.00003-temp2-epoch2-lr5e-5/checkpoint-60"
 python scripts/generate-distillation-data-offline.py \
