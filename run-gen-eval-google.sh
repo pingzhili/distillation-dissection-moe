20ae49f524bf99f089d60a554e15d1459c85ca23
Pingzhi Li <pingzhi@cs.unc.edu>
2025-05-08 18:18:13 -0400

    update

--- run-gen-eval-google.sh
+++ run-gen-eval-google.sh
@@ -45,7 +45,7 @@ for i in {0..7}; do
     PORT=$((23333 + i))
     python scripts/generate-distillation-data.py \
         --dataset_name="ServiceNow-AI/R1-Distill-SFT" --save_dir="data/r1-qwen-7b-mixed/" \
-        --model_name="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B" --max_tokens=8192 \
+        --model_name="Qwen/Qwen3-8B" --max_tokens=8192 \
         --shuffle=False --split_id=${i} --num_splits=8 --base_url="http://localhost:${PORT}/v1" &
 done
 
