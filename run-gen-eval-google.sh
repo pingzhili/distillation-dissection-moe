e1fd40c21c7019450a02c685a0721895d9306a62
Pingzhi Li <pingzhi@unites4.cs.unc.edu>
2025-05-15 02:10:07 -0400

    update

--- run-gen-eval-google.sh
+++ run-gen-eval-google.sh
@@ -75,27 +75,27 @@ export NCCL_P2P_DISABLE=1
 #       --task_name="gsm8k"&> "logs/qwen3-8b-antidistill-coef0.00003-temp2-head_proj0-epoch1-lr5e-5--checkpoint-120-gen-gsm8k-${i}.log" &
 # sleep 600
 
-SOURCE_MODEL="qwen3-8b-antidistill-coef0.00003-temp2-head_proj0-epoch1-lr5e-5-checkpoint-120"
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --config_file configs/zero3-8gpu-ga16.yaml \
-    --num_processes=8 \
-    --num_machines=1 \
-    --machine_rank=0 \
-    --main_process_port=23333 \
-    --mixed_precision=bf16 \
-    scripts/finetune-sft.py \
-    --base_model_name="meta-llama/Llama-3.2-1B" \
-    --output_dir="outputs/llama-3.2-1b-distill--$SOURCE_MODEL" \
-    --dataset_name="data/antidistill-exps/gsm8k/$SOURCE_MODEL.jsonl" \
-    --num_train_epochs=3 \
-    --batch_size_per_device=1 \
-    --gradient_accumulation_steps=16 &
+# SOURCE_MODEL="qwen3-8b-antidistill-coef0.00003-temp2-head_proj0-epoch1-lr5e-5-checkpoint-120"
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch --config_file configs/zero3-8gpu-ga16.yaml \
+#     --num_processes=8 \
+#     --num_machines=1 \
+#     --machine_rank=0 \
+#     --main_process_port=23333 \
+#     --mixed_precision=bf16 \
+#     scripts/finetune-sft.py \
+#     --base_model_name="meta-llama/Llama-3.2-1B" \
+#     --output_dir="outputs/llama-3.2-1b-distill--$SOURCE_MODEL" \
+#     --dataset_name="data/antidistill-exps/gsm8k/$SOURCE_MODEL.jsonl" \
+#     --num_train_epochs=3 \
+#     --batch_size_per_device=1 \
+#     --gradient_accumulation_steps=16 &
   
 SOURCE_MODEL="qwen3-8b-antidistill-coef0.00003-temp2-head_proj0-epoch1-lr5e-5-checkpoint-120"
 CUDA_VISIBLE_DEVICES=8,9,10,11,12,13,14,15 accelerate launch --config_file configs/zero3-8gpu-ga16.yaml \
     --num_processes=8 \
     --num_machines=1 \
     --machine_rank=0 \
-    --main_process_port=23333 \
+    --main_process_port=23334 \
     --mixed_precision=bf16 \
     scripts/finetune-sft.py \
     --base_model_name="google/gemma-3-1b-it" \
