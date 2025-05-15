3e993c67468307f5fdeccefe8cab08e241e18b96
Pingzhi Li <pingzhi@unites4.cs.unc.edu>
2025-05-15 02:54:55 -0400

    update

--- run-gen-eval-qwen3-8b.sh
+++ run-gen-eval-qwen3-8b.sh
@@ -1,7 +1,18 @@
 export PYTHONPATH=$PYTHONPATH:src
-export CUDA_VISIBLE_DEVICES=4
+export MODEL_PATH="outputs/qwen3-8b-antidistill-coef0.00003-temp2-head_proj0-epoch1-lr5e-5/checkpoint-60/lm_head.pt"
 
 
-MODEL_PATH="outputs/qwen3-8b-antidistill-coef0.00003-temp2-head_proj0-epoch1-lr5e-5/checkpoint-120/lm_head.pt"
-python scripts/generate-eval.py \
-    --model_path="$MODEL_PATH" --task_name="arcc"
+CUDA_VISIBLE_DEVICES=3 python scripts/generate-eval.py \
+    --model_path="$MODEL_PATH" --task_name="arcc" &> logs/qwen3-8b-antidistill-mixed-arcc.log &
+
+sleep 300
+
+CUDA_VISIBLE_DEVICES=5 python scripts/generate-eval.py \
+    --model_path="$MODEL_PATH" --task_name="csqa" &> logs/qwen3-8b-antidistill-mixed-csqa.log &
+
+# sleep 300
+
+# CUDA_VISIBLE_DEVICES=7 python scripts/generate-eval.py \
+#     --model_path="$MODEL_PATH" --task_name="gsm8k" &> logs/qwen3-8b-antidistill-mixed-gsm8k.log &
+
+# sleep 300
